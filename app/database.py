6409533691f51bda1030ff10898d552046b76dbe
dev-owdenmag <mag.owden@gmail.com>
2025-07-17 05:30:32 +0000

    Moved database details to environment variable.

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:Commando1+@localhost:5432/fastAPI"
+# Load environment variables from .env file
+load_dotenv()   
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+DATABASEURL = os.getenv('DATABASE_URL')
+                         
+engine = create_async_engine(DATABASEURL, echo=True)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
